23991262dcdf2cc2b844c426f8d7e79a9202199a
Akiman-sanbo <71792789+Akiman-sanbo@users.noreply.github.com>
2020-10-11 17:09:53 +0900

    修正しました

--- js/task2.js
+++ js/task2.js
@@ -3,38 +3,44 @@
 document.getElementById('Run').addEventListener('click',() => {
   const FizzText = document.getElementById('Fizz').value;
   const BuzzText = document.getElementById('Buzz').value;
-  const parent = document.getElementById('OutputParent');
-  while(parent.lastChild){
+  const parent = document.getElementById('outputParent');
+  while (parent.lastChild) {
     parent.removeChild(parent.lastChild);
   }
 
-  const isFizzInteger = Number.isInteger(FizzText);
-  console.log(isFizzInteger);
+  const isFizzInteger = Number.isInteger(Number(FizzText));
+  const isBuzzInteger = Number.isInteger(Number(BuzzText));
+
+  if (FizzText==="" || BuzzText === "") { 
+    const New = document.createElement('p');
+    New.textContent = '整数値を入力してください';
+    const div  = document.getElementById('outputParent');
+    div.appendChild(New);
+    return false;
+  } else if (isFizzInteger && isBuzzInteger){  
+     for (let i = 1 ; i <100 ; i++) {
+        if ( i % FizzText === 0 && i% BuzzText === 0) {
+           const New = document.createElement('p');
+           New.textContent = `FizzBuzz${i}`;
+           const div  = document.getElementById('outputParent');
+           div.appendChild(New);
+         } else if (i % FizzText === 0) {
+            const New = document.createElement('p');
+            New.textContent = `Fizz${i}`; 
+            const div  = document.getElementById('outputParent');
+            div.appendChild(New);
+         } else if (i % BuzzText === 0) {
+            const New = document.createElement('p');
+            New.textContent = `Buzz${i}`; 
+            const div  = document.getElementById('outputParent');
+            div.appendChild(New);
+         }
+      }
+   } else { 
+   const New = document.createElement('p');
+   New.textContent = '整数値を入力してください';
+   const div  = document.getElementById('outputParent');
+   div.appendChild(New);
+   return false;
+   }
 });
-//   if(isFizzInteger && isBuzzInteger){  
-//      for(let i = 1 ; i <100 ; i++){
-//         if( i % FizzText === 0 && i% BuzzText === 0){
-//            let New = document.createElement('p');
-//            New.textContent = `FizzBuzz${i}`;
-//            let div  = document.getElementById('OutputParent');
-//            div.appendChild(New);
-//          } else if(i % FizzText === 0){
-//             let New = document.createElement('p');
-//             New.textContent = `Fizz${i}`; 
-//             let div  = document.getElementById('OutputParent');
-//             div.appendChild(New);
-//          } else if(i % BuzzText === 0){
-//             let New = document.createElement('p');
-//             New.textContent = `Buzz${i}`; 
-//             let div  = document.getElementById('OutputParent');
-//             div.appendChild(New);
-//          }
-//       }
-//     } else { 
-//    let New = document.createElement('p');
-//    New.textContent = '整数値を入力してください';
-//    let div  = document.getElementById('OutputParent');
-//    div.appendChild(New);
-//    return false;
-//    }
-// });
